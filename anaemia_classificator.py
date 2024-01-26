c42943a5b1fabdbf98c6a7840071fcf736dca97e
Rocco Piazzolla <roccopiazzo2001@gmail.com>
2024-01-26 17:31:59 +0100

    reduced dataset for test, segmtentation algorithm modified

--- anaemia_classificator.py
+++ anaemia_classificator.py
@@ -10,77 +10,108 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Funzione per l'estrazione delle feature da un'immagine
-def extract_features(image_path):
+def extract_features(congiuntiva_region):
     print("chiamata a extract features...")
-    print("Percorso immagine: ", image_path)
+    #print("Percorso immagine: ", image_path)
+    
     # Caricamento dell'immagine utilizzando la libreria skimage
-    image = cv2.imread(image_path)
+    # passo l'immagine segmentata
+    image = congiuntiva_region
 
     # gestisce il caso in cui non legge l'immagine
     if image is None:
         print(f"Errore: Impossibile leggere l'immagine {image_path}")
         return None
 
-    # Conversione in scala di grigi, se l'immagine non è già in scala di grigi
-    gray_image = color.rgb2gray(image)
+    # Estrai la componente rossa dell'immagine
+    red_channel = image[:, :, 2]
 
-    # Esempio di estrazione di feature: utilizziamo il numero medio di edge rilevati con Canny
-    canny_edges = feature.canny(gray_image)
-    canny_edges_mean = canny_edges.mean()
+    # Calcola la media dei livelli di emoglobina (o la tua metrica desiderata)
+    hemoglobin_level = np.mean(red_channel)
 
-    # Visualizza l'immagine originale e i bordi rilevati con Canny
-    plt.subplot(1, 2, 1)
-    plt.imshow(image)
-    plt.title('Immagine originale')
+    print("livello emoglobina: ", hemoglobin_level);
 
-    plt.subplot(1, 2, 2)
-    plt.imshow(canny_edges, cmap='gray')
-    plt.title('Bordi con Canny')
+    # Visualizza l'immagine originale e la componente rossa
+    # plt.subplot(1, 2, 1)
+    # plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    # plt.title('Immagine originale')
 
-    plt.show()
+    # plt.subplot(1, 2, 2)
+    # plt.imshow(red_channel, cmap='gray')
+    # plt.title('Componente rossa')
 
-    # Puoi aggiungere ulteriori feature qui in base alle tue esigenze
-    # ...
+    # plt.show()
 
     # Restituzione delle feature estratte come un dizionario
     return {
-        'canny_edges_mean': canny_edges_mean,
-        # Aggiungi altre feature qui se necessario
+        'hemoglobin_level': hemoglobin_level,
+        # Aggiungi altre feature qui in base alle tue esigenze
     }
 
 # Funzione per estrarre la parte della congiuntiva da un'immagine
 def segment_congiuntiva(original_image):
-    print("chiamata a segmentcongiutiva...");
-    # Converti l'immagine in scala di grigi
-    gray_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    print("chiamata a segmentcongiutiva...")
 
-    # Applica un filtro di smooth per ridurre il rumore
-    blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+    # Estrai il canale V (value) dalla rappresentazione HSV dell'immagine
+    hsv_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2HSV)
+    v_channel = hsv_image[:,:,2]
 
-    # Esegui la segmentazione utilizzando il metodo di Otsu
-    _, binary_mask = cv2.threshold(blurred_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # Fase 1: Rimuovi le parti scure
+    _, binary_mask_dark = cv2.threshold(v_channel, 100, 255, cv2.THRESH_BINARY_INV)
+    binary_mask_dark = cv2.morphologyEx(binary_mask_dark, cv2.MORPH_OPEN, kernel=np.ones((5, 5), np.uint8))
+    binary_mask_dark = cv2.morphologyEx(binary_mask_dark, cv2.MORPH_CLOSE, kernel=np.ones((5, 5), np.uint8))
 
-    # Trova i contorni nella maschera binaria
-    contours, _ = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Fase 2: Rimuovi le parti chiare
+    _, binary_mask_light = cv2.threshold(v_channel, 200, 255, cv2.THRESH_BINARY)
+    binary_mask_light = cv2.morphologyEx(binary_mask_light, cv2.MORPH_OPEN, kernel=np.ones((5, 5), np.uint8))
+    binary_mask_light = cv2.morphologyEx(binary_mask_light, cv2.MORPH_CLOSE, kernel=np.ones((5, 5), np.uint8))
 
-    # Trova il contorno più grande (presumibilmente la congiuntiva)
-    largest_contour = max(contours, key=cv2.contourArea)
+    # Combinazione delle maschere utilizzando l'operatore AND
+    binary_mask_combined = cv2.bitwise_and(binary_mask_dark, binary_mask_light)
 
-    # Crea una maschera vuota delle stesse dimensioni dell'immagine originale
-    mask = np.zeros_like(original_image)
+    # Visualizza le maschere binarie
+    plt.subplot(2, 3, 1)
+    plt.imshow(binary_mask_dark, cmap='gray')
+    plt.title('Maschera Scure')
 
-    # Disegna il contorno sulla maschera
-    cv2.drawContours(mask, [largest_contour], -1, (255, 255, 255), thickness=cv2.FILLED)
+    plt.subplot(2, 3, 2)
+    plt.imshow(binary_mask_light, cmap='gray')
+    plt.title('Maschera Chiare')
 
-    # Estrai la parte dell'immagine originale corrispondente alla maschera
-    congiuntiva_region = cv2.bitwise_and(original_image, mask)
+    plt.subplot(2, 3, 3)
+    plt.imshow(binary_mask_combined, cmap='gray')
+    plt.title('Maschera Combinata')
+
+    # Trova i contorni nella maschera binaria combinata
+    contours, _ = cv2.findContours(binary_mask_combined, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Filtra i contorni in base a proprietà morfologiche (ad esempio, lunghezza)
+    min_contour_length = 100  # Regola questo valore in base alle tue esigenze
+    filtered_contours = [contour for contour in contours if cv2.arcLength(contour, True) > min_contour_length]
+
+    # Verifica se ci sono contorni prima di procedere
+    if filtered_contours:
+        # Trova il contorno più grande tra quelli filtrati
+        largest_contour = max(filtered_contours, key=cv2.contourArea)
+
+        # Crea una maschera vuota delle stesse dimensioni dell'immagine originale
+        mask = np.zeros_like(original_image)
+
+        # Disegna il contorno sulla maschera
+        cv2.drawContours(mask, [largest_contour], -1, (255, 255, 255), thickness=cv2.FILLED)
+
+        # Estrai la parte dell'immagine originale corrispondente alla maschera
+        congiuntiva_region = cv2.bitwise_and(original_image, mask)
+    else:
+        print("Nessun contorno trovato.")
+        congiuntiva_region = np.zeros_like(original_image)  # Immagine vuota
 
     # Visualizza l'immagine originale e la parte della congiuntiva
-    plt.subplot(1, 2, 1)
+    plt.subplot(2, 3, 4)
     plt.imshow(cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
     plt.title('Immagine originale')
 
-    plt.subplot(1, 2, 2)
+    plt.subplot(2, 3, 5)
     plt.imshow(cv2.cvtColor(congiuntiva_region, cv2.COLOR_BGR2RGB))
     plt.title('Parte della congiuntiva')
 
@@ -99,6 +130,9 @@ print("Percorso cartella: ", image_folder_path);
 features_list = []
 labels_list = []
 
+# Definizione della soglia per i livelli di emoglobina
+soglia = 0.5  # Modifica questo valore in base ai tuoi requisiti
+
 # Ciclo attraverso le immagini nella cartella
 for filename in os.listdir(image_folder_path):
     print("entro nel ciclo for...");
@@ -107,7 +141,7 @@ for filename in os.listdir(image_folder_path):
         # Costruzione del percorso completo dell'immagine
         image_path = os.path.join(image_folder_path, filename)
 
-        print("percorso immagine: ", image_path);
+        # print("percorso immagine: ", image_path);
 
         # Caricamento dell'immagine utilizzando OpenCV
         original_image = cv2.imread(image_path)
@@ -116,12 +150,10 @@ for filename in os.listdir(image_folder_path):
         congiuntiva_region = segment_congiuntiva(original_image)
 
         # Esegui l'estrazione delle feature sulla parte della congiuntiva
-        # features = extract_features(congiuntiva_region)
-        features = extract_features(image_path)
+        features = extract_features(congiuntiva_region)
 
-        # Determina l'etichetta in base alle tue condizioni
-        # Ad esempio, se il nome del file contiene 'congiuntiva', l'etichetta potrebbe essere 1, altrimenti 0
-        label = 1 if 'congiuntiva' in filename.lower() else 0
+        # Determina l'etichetta in base ai livelli di emoglobina
+        label = 1 if features['hemoglobin_level'] > soglia else 0  # Personalizza la soglia secondo necessità
 
         # Aggiunta dell'etichetta se necessario
         labels_list.append(label)
@@ -171,6 +203,8 @@ print(f'Accuracy: {accuracy}')
 print(f'Classification Report:\n{report}')
 print(f'Confusion Matrix:\n{conf_matrix}')
 
+# servono per capire quanto bene lavora il modello di regressione logistica
+# matrice di confusione valuta i falsi positivi e negativi e i veri positivi e negativi
 # Visualizzazione della matrice di confusione
 plt.figure(figsize=(8, 6))
 sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=['Classe 0', 'Classe 1'], yticklabels=['Classe 0', 'Classe 1'])
@@ -183,6 +217,7 @@ plt.show()
 fpr, tpr, thresholds = roc_curve(y_test, model.predict_proba(X_test)[:, 1])
 roc_auc = auc(fpr, tpr)
 
+# fornisce una visione complessiva delle prestazioni del modello al variare della soglia di decisione
 # Visualizzazione della curva ROC
 plt.figure(figsize=(8, 6))
 plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
