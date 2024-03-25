03c620bfb9c42101412c8952c322676edfa50e16
Rocco Piazzolla <roccopiazzo2001@gmail.com>
2024-03-25 23:08:07 +0100

    code optimization and code cleanup

--- extractor.py
+++ extractor.py
@@ -1,29 +1,18 @@
 import numpy as np
-from skimage import io, color, filters, morphology
-import matplotlib.pyplot as plt
+from skimage import color, filters, morphology
 
-from skimage.filters import gabor
-from skimage import data, color
-from scipy import ndimage as ndi
+# valori di riferimento 
+MIN_RED_LEVEL = 7000     # soglia che indica il valore minimo di rosso per indicare che l'occhio è sano
+MIN_WHITE_LEVEL = 0.0003   # soglia che indica il valore minimo di bianco per indicare che l'occhio non è sano
 
 def extract_features(image):
-    # plt.imshow(image)
-    # plt.axis('off')  # Nasconde gli assi
-    # plt.show()
-    
-    # Altre caratteristiche...
-    
     # Estrae la percentuale di rosso presente nell'immagine
     red_level = extract_red_level(image)
-
-    # print("percentuale di rosso: ", red_level)
 
     # estrai le zone chiare dalle congiutive, di conseguenza il numero di pixel bianchi presente nell'immagine
     # ritorna la percentuale di bianco presente nell'immagine
     white_level = extract_white_level(image);
 
-    # print("percentuale di bianco: ", white_level)
-    
     # Altre caratteristiche...
     # print("\nlivello rosso: ", red_level)
     # print("livello bianco: ", white_level)
@@ -35,11 +24,6 @@ def extract_red_level(image):
     # Estrai il canale rosso dall'immagine
     red_channel = image[:, :, 0]
 
-    # plt.imshow(red_channel, cmap='gray')  # Usa 'gray' colormap per visualizzare in scala di grigi
-    # plt.axis('off')  # Nasconde gli assi
-    # plt.title("Canale Rosso")
-    # plt.show()
-
     # Calcola la quantità totale di rosso nell'immagine
     total_red = np.sum(red_channel)
 
@@ -47,9 +31,6 @@ def extract_red_level(image):
     total_pixels = image.shape[0] * image.shape[1]
     red_percentage = (total_red / total_pixels) * 100
 
-    # print("Quantità totale di rosso nell'immagine:", total_red)
-    # print("Percentuale di rosso nell'immagine:", red_percentage)
-    
     return red_percentage
 
 # da sistemare ma funziona
@@ -74,7 +55,16 @@ def extract_white_level(image):
     total_pixels = white_mask.size
     white_percentage = (num_white_regions / total_pixels) * 100
 
-    # print("Numero di zone bianche:", num_white_regions)
-    # print("Percentuale di bianco nell'immagine:", white_percentage)
-
     return white_percentage
+
+def assign_label(features):
+    # 0 red_level
+    # 1 white_level
+
+    # se il minimo rosso non viene raggiunto
+    # e il minimo bianco viene raggiunto 
+    # l'occhio non è sano
+    if features[0] < MIN_RED_LEVEL and features[1] > MIN_WHITE_LEVEL:
+        return 1  # Assegna la label 1 se il livello di rosso è inferiore a 10 e il livello di bianco è superiore a 10
+    else:
+        return 0  # Altrimenti assegna la label 0
--- label_assignator.py
+++
@@ -1,7 +0,0 @@
-def assign_label(features):
-    # 0 red_level
-    # 1 white_level
-    if features[0] < 7000 and features[1] > 0.0003:
-        return 1  # Assegna la label 1 se il livello di rosso è inferiore a 10 e il livello di bianco è superiore a 10
-    else:
-        return 0  # Altrimenti assegna la label 0
